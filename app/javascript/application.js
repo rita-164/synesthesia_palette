de97424f287e99664b02333839622497ec6a3840
ryota <ryttkd514@gmail.com>
2022-05-07 21:09:23 +0900

    install_gems

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import "bootstrap";
+import "../stylesheets/application";
