fe0f3cc1936287d8b43226d6f0f745ad4f63764f
ryota <ryttkd514@gmail.com>
2022-05-16 18:39:50 +0900

    createsynesthesia

--- app/controllers/human_name_synesthesia_controller.rb
+++ app/controllers/human_name_synesthesia_controller.rb
@@ -1,5 +1,49 @@
 class HumanNameSynesthesiaController < ApplicationController
     def index
-        @human_name_synesthesias = HumanNameSynesthesium.all.includes(:user).order(created_at: :desc)
-      end
+        @human_name_synesthesium = HumanNameSynesthesium.all.includes(:user).order(created_at: :desc)
+    end
+
+    def new
+        @human_name_synesthesia = HumanNameSynesthesium.new
+    end
+    
+    def create
+        @human_name_synesthesia = current_user.human_name_synesthesia.build(human_name_synesthesia_params)
+        if @human_name_synesthesia.save
+          redirect_to human_name_synesthesia_path, success: t('defaults.message.created', item: HumanNameSynesthesium.model_name.human)
+        else
+          flash.now['danger'] = t('defaults.message.not_created', item: HumanNameSynesthesium.model_name.human)
+          render :new
+        end
+    end
+    
+    def show
+        @human_name_synesthesia = HumanNameSynesthesium.find(params[:id])
+    end
+
+    def edit
+        @human_name_synesthesia = HumanNameSynesthesium.find(params[:id])
+    end
+    
+    def update
+        @human_name_synesthesia = current_user.human_name_synesthesia.find(params[:id])
+        if @human_name_synesthesia.update(human_name_synesthesia_params)
+          redirect_to human_name_synesthesia_path, success: t('defaults.message.updated', item: HumanNameSynesthesium.model_name.human)
+        else
+          flash.now['danger'] = t('defaults.message.not_updated', item: HumanNameSynesthesium.model_name.human)
+          render :edit
+        end
+    end
+    
+    def destroy
+        @human_name_synesthesia = current_user.human_name_synesthesia.find(params[:id])
+        @human_name_synesthesia.destroy!
+        redirect_to human_name_synesthesia_path, success: t('defaults.message.deleted', item: HumanNameSynesthesium.model_name.human)
+    end
+    
+    private
+    
+    def human_name_synesthesia_params
+        params.require(:human_name_synesthesium).permit(:name, :color)
+    end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,10 +4,8 @@ class SearchesController < ApplicationController
     def search
       @range = params[:range]
   
-      if @range == "User"
-        @users = User.looks(params[:search], params[:word])
-      else
-        @books = Book.looks(params[:search], params[:word])
+      if @range == "Name"
+        @users = HumanNameSynesthesium.looks(params[:search], params[:word])
       end
     end  
 end
