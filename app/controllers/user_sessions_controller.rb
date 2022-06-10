db015c06b98571c88dfcf782a715dfb0fc251c25
ryota <ryttkd514@gmail.com>
2022-06-10 17:35:04 +0900

    fixlogin

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,20 +1,19 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
 
   def new; end
 
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, success: t('.success')
+      redirect_back_or_to root_path, dark: t('.success')
     else
-      flash.now[:danger] = t('.fail')
+      flash.now[:dark] = t('.fail')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, success: t('.success')
+    redirect_to root_path, dark: t('.success')
   end
 end
